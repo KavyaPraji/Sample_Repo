fb789fe163a9bbbdb9da2717146dc345144337d5
praje <praje@192.168.137.235>
2024-05-29 16:04:05 +0530

    ProductPage added POM class

---
+++ Testing_Training/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -0,0 +1,16 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage {
+	
+	
+	@FindBy(xpath="//img[@title='Create Product...']")
+	private WebElement createPdtBtn;
+
+	public WebElement getCreatePdtBtn() {
+		return createPdtBtn;
+	}
+
+}
